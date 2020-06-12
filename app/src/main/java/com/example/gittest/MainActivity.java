ce297d6dc11b205dc8e04e515fe7c1df6f860fa9
bljboy <bljboy55@gmail.com>
2020-06-12 21:10:11 +0800

    bao

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        森赛弗萨福塞菲斯
+//
     }
 }
